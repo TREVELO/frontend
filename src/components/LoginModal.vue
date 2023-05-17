751f03f931a127f63054d309d60acbebfbe23c55
Shin-sangwon <dnjsml10@gmail.com>
2023-05-17 17:50:15 +0900

    feat : 토큰 발급 구현

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -57,6 +57,7 @@
   
 <script>
 import axios from "axios";
+import {mapActions} from "vuex";
 
 export default {
     data() {
@@ -70,6 +71,8 @@ export default {
         }
     },
     methods: {
+         ...mapActions("memberStore", ["setToken", "getToken"]), // Vuex의 memberStore 모듈의 setToken 액션을 매핑
+
         validationCheck() {
             this.errors = [];
             if(!this.form.loginId) {
@@ -82,26 +85,40 @@ export default {
                 this.errorshow = true;
             }
         },
-        async login() {
-            console.log("login request");
-            console.log("id = " + this.form.loginId + " password " + this.form.loginPassword);
-            this.validationCheck();
-            console.log(this.errors);
-            if(this.errors.length === 0) {
-                console.log(this.form);
+async login() {
+  console.log("login request");
+  console.log("id = " + this.form.loginId + " password " + this.form.loginPassword);
+  this.validationCheck();
+  console.log(this.errors);
+  if (this.errors.length === 0) {
+    console.log(this.form);
 
-                await axios.post('http://localhost/api/v1/member/login', this.form)
-                .then((res) => {
-                    console.log(res.data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-            }
+    try {
+      const response = await axios.post('http://localhost/api/v1/member/login', this.form);
+      console.log(response.data);
 
-            
-        }
+      const token = response.data; // 응답에서 토큰 추출
+
+      // 토큰을 세션 스토리지에 저장
+      sessionStorage.setItem("token", token);
+
+      // Vuex의 memberStore 모듈의 setToken 액션을 호출하여 토큰 설정
+      this.$store.dispatch("memberStore/setToken", token);
+      console.log(token);
+      console.log("token is");
+      console.log(this.$store.getters["memberStore/getToken"]);
+      console.log(sessionStorage.getItem("token"));
+    } catch (error) {
+      console.log(error.response.data);
+      this.errors = [];
+      this.errors.push(error.response.data);
+      if (!this.errorshow) {
+        this.errorshow = true;
+      }
     }
+  }
+}
+  }
 }
 </script>
 
@@ -191,7 +208,7 @@ export default {
 
 #errorMsg{
     color: crimson;
-    font-size: 15px;
+    font-size: 13px;
     list-style: none;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+
+import memberStore from "@/store/modules/memberStore";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {},
-    modules: {},
+    modules: {
+        memberStore,
+    },
+    plugins: [
+        createPersistedState({
+            storage: sessionStorage,
+        }),
+    ],
 });
