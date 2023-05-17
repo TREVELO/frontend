e7b841865f394a272e75fe971abd314dfdae2044
이도하 <victoryddh5@naver.com>
2023-05-17 17:58:16 +0900

    board

---
+++ src/components/Sub-Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <ul style="list-style: none; text-align: center; margin-top: 30px;">
+            <li><button class="btn-btn"><router-link :to="{ name: 'AppHomeVue' }">
+                        <font-awesome-icon icon="fa-solid fa-house" style="color: #0a0a0a;" /><b style="color: #0a0a0a;">
+                            홈</b>
+                    </router-link></button>
+            </li>
+            <li><button><router-link :to="{ name: 'board' }"><b style="color: #0a0a0a;"><font-awesome-icon
+                                icon="fa-solid fa-chess-board" style="color: #0a0a0a;" />
+                            게시판</b></router-link></button></li>
+            <li><button class="btn-btn"><b>공지사항</b></button></li>
+            <li><button class="btn-btn"><b>숙소</b></button></li>
+            <li><button class="btn-btn"><b>숙소</b></button></li>
+            <li><button class="btn-btn"><b>게시판</b></button></li>
+            <li><button class="btn-btn"><b>게시판</b></button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+li {
+    margin-left: 15px;
+    margin-right: 15px;
+    display: inline-block;
+}
+
+button {
+    font-size: large;
+    border: none;
+    font-family: 'Open Sans', sans-serif;
+    background-color: transparent;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Karla&family=Open+Sans&display=swap');
+</style>
---
+++ src/views/AppBoard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">
+        <div id="header">
+            <main-header />
+            <sub-header />
+            <b>AppBoard창</b>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+import MainHeader from "@/components/Main-Header.vue"
+import SubHeader from "@/components/Sub-Header.vue";
+
+export default {
+    name: 'AppBoard',
+    components: {
+        'main-header': MainHeader,
+        'sub-header': SubHeader,
+    }
+}
+</script>
+
+<style scoped></style>
