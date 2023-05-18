62e06413b660389a7622dd5ca8533061adb8f9c8
Shin-sangwon <dnjsml10@gmail.com>
2023-05-18 13:36:23 +0900

    fix

--- src/components/AppBoard/BoardWrite.vue
+++ src/components/AppBoard/BoardWrite.vue
@@ -13,15 +13,25 @@
         <b-row class="mb-1">
             <b-col style="text-align: left">
                 <b-form @submit="onSubmit">
-
                     <b-form-group id="title-group" label="제목:" label-for="title">
-                        <b-form-input id="title" v-model="article.title" type="text" required
-                            placeholder="제목 입력..."></b-form-input>
+                        <b-form-input
+                            id="title"
+                            v-model="article.title"
+                            type="text"
+                            required
+                            placeholder="제목 입력..."
+                        ></b-form-input>
                     </b-form-group>
 
                     <b-form-group id="content-group" label="내용:" label-for="content">
-                        <b-form-textarea id="content" v-model="article.content" placeholder="내용 입력..." rows="10"
-                            max-rows="15" required></b-form-textarea>
+                        <b-form-textarea
+                            id="content"
+                            v-model="article.content"
+                            placeholder="내용 입력..."
+                            rows="10"
+                            max-rows="15"
+                            required
+                        ></b-form-textarea>
                     </b-form-group>
 
                     <b-button type="submit" variant="primary" class="m-1">글작성</b-button>
@@ -34,7 +44,7 @@
 </template>
 
 <script>
-import axiosInstance from '@/api/axiosInstance';
+import axiosInstance from "@/api/axiosInstance";
 
 export default {
     name: "BoardWrite",
@@ -43,33 +53,33 @@ export default {
             article: {
                 title: "",
                 content: "",
-                boardTag: ""
-            }
-        }
+                boardTag: "",
+            },
+        };
     },
     methods: {
         onSubmit(event) {
             event.preventDefault();
 
-            axiosInstance.post('http://localhost/api/v1/board/write', this.article)
+            axiosInstance
+                .post("http://localhost/api/v1/board/write", this.article)
                 .then((res) => {
-                    console.log(res.data)
-                    this.moveList()
+                    console.log(res.data);
+                    this.moveList();
                 })
                 .catch((err) => {
-                    console.log(err)
-                })
+                    console.log(err);
+                });
         },
         onReset() {
-            this.article.title = "",
-                this.article.content = ""
+            (this.article.title = ""), (this.article.content = "");
             // window.location.reload(true);
         },
         moveList() {
             this.$router.push({ name: "BoardList" });
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
 <style></style>
--- src/store/modules/memberStore.js
+++ src/store/modules/memberStore.js
@@ -5,7 +5,7 @@ const memberStore = {
     namespaced: true,
     state: {
         token: null, // 토큰 상태
-        userinfo: null,
+        userinfo: null, // 사용자 정보 상태
     },
     mutations: {
         SET_TOKEN(state, token) {
@@ -19,13 +19,10 @@ const memberStore = {
         setToken({ commit }, token) {
             commit("SET_TOKEN", token); // SET_TOKEN 뮤테이션을 호출하여 토큰 설정
         },
-        decodeToken({ commit, state }) {
+        decodeToken({ state }) {
             if (state.token) {
                 const decodedToken = jwtDecode(state.token); // 토큰 디코드
-                const userinfo = decodedToken.memberId; // 사용자 정보 추출
-                console.log(userinfo)
-                commit("SET_USERINFO", userinfo); // SET_USERINFO 뮤테이션을 호출하여 사용자 정보 설정
-                return userinfo; // 추출한 사용자 정보 반환
+                return decodedToken;
             }
             return null;
         },
