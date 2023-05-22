38214d6f4bf76856daa63484d7ea605d273d35e5
이도하 <victoryddh5@naver.com>
2023-05-22 16:28:44 +0900

    home recommendation

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -10,13 +10,54 @@
         현재 위치 기반 추천 장소 찾기
       </button>
       <div v-else>
-        GPS 동의 상태
+        <div style="margin-top: 30px;"><b style="color: blue; font-size: 25px;">추천 관광지</b><button class="btn btn-primary"
+            style="margin-left: 10px; border: none;" @click="makeRandom(nearAttractions, showAttractions)">새로
+            보기</button></div>
+        <div style="margin-top: 50px;">
+          <div class="card" style="width: 20rem; height: 25rem; display: inline-block;" v-for="(show) in showAttractions"
+            :key="show.contentId">
+            <img :src="show.first_image" class="" alt="" style="margin-top: 30px; width: 200px; height: 200px;">
+            <div class="card-body" style="height: ;">
+              <p class="card-text"><b>{{ show.title }}</b></p>
+              <p class="card-text"><b>{{ show.addr }}</b></p>
+            </div>
+          </div>
+        </div>
+        <!--  -->
+        <div style="margin-top: 30px;"><b style="color: blue; font-size: 25px;">추천 쇼핑</b><button class="btn btn-primary"
+            style="margin-left: 10px; border: none;" @click="makeRandom(nearShopping, showShopping)">새로
+            보기</button></div>
+        <div style="margin-top: 50px;">
+          <div class="card" style="width: 20rem; height: 25rem; display: inline-block;" v-for="(show) in showShopping"
+            :key="show.contentId">
+            <img :src="show.first_image" class="" alt="" style="margin-top: 30px; width: 200px; height: 200px;">
+            <div class="card-body" style="height: ;">
+              <p class="card-text"><b>{{ show.title }}</b></p>
+              <p class="card-text"><b>{{ show.addr }}</b></p>
+            </div>
+          </div>
+        </div>
+        <!--  -->
+        <div style="margin-top: 30px;"><b style="color: blue; font-size: 25px;">추천 음식점</b><button class="btn btn-primary"
+            style="margin-left: 10px; border: none;" @click="makeRandom(nearRestaurant, showRestaurant)">새로
+            보기</button></div>
+        <div style="margin-top: 50px;">
+          <div class="card" style="width: 20rem; height: 25rem; display: inline-block;" v-for="(show) in showRestaurant"
+            :key="show.contentId">
+            <img :src="show.first_image" class="" alt="" style="margin-top: 30px; width: 200px; height: 200px;">
+            <div class="card-body" style="height: ;">
+              <p class="card-text"><b>{{ show.title }}</b></p>
+              <p class="card-text"><b>{{ show.addr }}</b></p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axiosInstance from "@/api/axiosInstance";
 import MainHeader from "@/components/Main-Header.vue"
 import SubHeader from "@/components/Sub-Header.vue";
 
@@ -29,7 +70,15 @@ export default {
   data() {
     return {
       position: {},
-      isPositionAccesable: false
+      isPositionAccesable: false,
+      nearAttractions: [],
+      nearShopping: [],
+      nearRestaurant: [],
+      attractions: [],
+      distances: [],
+      showAttractions: [],
+      showShopping: [],
+      showRestaurant: []
     }
   },
   methods: {
@@ -48,9 +97,80 @@ export default {
       console.log(this.position.latitude)
       console.log("현재 longitude 좌표")
       console.log(this.position.longitude)
+
+      axiosInstance.post("http://localhost/api/v1/attraction/search?title=&sidoCode=&gugunCode=&contentType=")
+        .then((res) => {
+          this.attractions = res.data;
+          console.log(this.attractions)
+
+          for (let index = 0; index < this.attractions.length; index++) {
+            let nowAttraction = this.attractions[index];
+
+            if (nowAttraction.first_image !== "") {
+              if (nowAttraction.contentType == 12) {
+                if (this.getDistance(this.position.latitude, this.position.longitude, nowAttraction.latitude, nowAttraction.longitude) < 20) {
+                  this.nearAttractions.push(nowAttraction);
+                }
+              } else if (nowAttraction.contentType == 38) {
+                if (this.getDistance(this.position.latitude, this.position.longitude, nowAttraction.latitude, nowAttraction.longitude) < 20) {
+                  this.nearShopping.push(nowAttraction);
+                }
+              } else if (nowAttraction.contentType == 39) {
+                if (this.getDistance(this.position.latitude, this.position.longitude, nowAttraction.latitude, nowAttraction.longitude) < 20) {
+                  this.nearRestaurant.push(nowAttraction);
+                }
+              }
+            }
+          }
+
+          this.makeRandom(this.nearAttractions, this.showAttractions);
+          this.makeRandom(this.nearShopping, this.showShopping);
+          this.makeRandom(this.nearRestaurant, this.showRestaurant)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     },
     gelocationError() {
       alert("위치 정보 권한을 허용해 주십시오.")
+    },
+    getDistance(lat1, lon1, lat2, lon2) {
+      var lat1p = parseFloat(lat1);
+      var lon1p = parseFloat(lon1);
+      var lat2p = parseFloat(lat2);
+      var lon2p = parseFloat(lon2);
+
+      var R = 6371;
+
+      var dLat = (lat2p - lat1p) * Math.PI / 180;
+      var dLon = (lon2p - lon1p) * Math.PI / 180;
+
+      var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c;
+
+      return d;
+
+    },
+    makeRandom(originalArray, showArray) {
+      var array = [];
+      var num = originalArray.length;
+
+      while (array.length < 5) {
+        var k = Math.floor(Math.random() * num);
+
+        if (!array.includes(k)) {
+          array.push(k);
+        }
+      }
+
+      showArray.splice(0, showArray.length);
+
+      for (var i = 0; i < 5; i++) {
+        showArray.push(originalArray[array[i]]);
+      }
+      console.log(showArray)
     }
   }
 }
