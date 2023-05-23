1d200f8dbbb9aaf19074693511cd5cf968127c86
Shin-sangwon <dnjsml10@gmail.com>
2023-05-23 17:50:36 +0900

    fix : subheader

--- src/components/Sub-Header.vue
+++ src/components/Sub-Header.vue
@@ -40,42 +40,15 @@
             <li>
                 <button class="btn-btn">
                     <router-link :to="{ name: 'room' }"
-                        ><b><font-awesome-icon icon="fa-solid fa-hotel" />숙소</b></router-link
-                    >
-                </button>
-            </li>
-            <li>
-                <button>
-                    <router-link :to="{ name: 'board' }"
                         ><b style="color: #0a0a0a"
                             ><font-awesome-icon
-                                icon="fa-solid fa-chess-board"
+                                icon="fa-solid fa-hotel"
                                 style="color: #0a0a0a"
-                            />
-                            게시판</b
+                            />숙소</b
                         ></router-link
                     >
                 </button>
             </li>
-            <li>
-                <button>
-                    <router-link :to="{ name: 'map' }"
-                        ><b style="color: #0a0a0a"
-                            ><font-awesome-icon
-                                icon="fa-solid fa-plane"
-                                style="color: #0a0a0a"
-                            />관광지</b
-                        ></router-link
-                    >
-                </button>
-            </li>
-
-            <li>
-                <button class="btn-btn">
-                    <b><font-awesome-icon icon="fa-solid fa-hotel" />숙소</b>
-                </button>
-            </li>
-
             <li>
                 <button>
                     <router-link :to="{ name: 'favorite' }"
