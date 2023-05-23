67988763d64014c4079ff5d25096bd139d585916
이도하 <victoryddh5@naver.com>
2023-05-24 03:31:09 +0900

    board comment getlist completed

--- src/components/AppBoard/BoardView.vue
+++ src/components/AppBoard/BoardView.vue
@@ -20,18 +20,47 @@
             v-if="article.memberId === userinfo.id">글삭제</b-button>
         </b-col>
       </b-row>
-      <b-row class="mb-1">
-        <b-col>
+      <!-- <b-row class="mb-1"> -->
+      <!-- <b-col>
           <b-card
-            :header-html="`<h3>${article.boardId}.
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ${article.title} [${article.hit}]</h3><div><h6>${article.content}</div><div>${article.createdat}</h6></div>`"
+            :header-html="`<h3>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ${article.title} [${article.hit}]</h3><div><h6>${article.content}</div><div>${article.createdat}</h6></div>`"
             class="mb-2" border-variant="dark" no-body>
-            <!-- <b-card-body class="text-left">
+            <b-card-body class="text-left">
             <div v-html="message"></div>
-          </b-card-body> -->
+          </b-card-body>
           </b-card>
-        </b-col>
-      </b-row>
+        </b-col> -->
+      <div style="margin-top: 30px; padding-bottom: 15px;">
+        <div style="display: flex; justify-content: space-between;">
+          <p style="text-align: left; font-size: 35px;">{{ article.title }}</p>
+          <p style="text-align: right; vertical-align: middle;" v-if="!article.updatedat">{{ article.createdat }}</p>
+          <p style="text-align: right; vertical-align: middle;" v-else>{{ article.updatedat }}</p>
+        </div>
+      </div>
+
+      <div style="margin-top: 30px; border-bottom: solid 0.5px gray; padding-bottom: 15px;">
+        <p style="text-align: left; font-size: 20px;">{{ article.content }}</p>
+      </div>
+
+      <div style="margin-top: 30px; text-align: left;">
+        총 {{ comments.length }}의 댓글이 있습니다.
+        <div style="margin-top: 20px;">
+          <textarea style="width: 100%; height: 120px; border-radius: 8px;" v-model="newComment"
+            placeholder="댓글을 작성하세요"></textarea>
+          <div style="text-align: right;">
+            <button class="btn btn-outline-dark">댓글 작성</button>
+          </div>
+        </div>
+
+      </div>
+      <div v-for="(comment, index) in comments" :key="comment.id">
+        <p>{{ index + 1 }}번째 댓글: {{ comment.content }}</p>
+
+      </div>
+
+
+
+      <!-- </b-row> -->
     </b-container>
   </div>
 </template>
@@ -47,6 +76,8 @@ export default {
     return {
       article: {},
       userinfo: [],
+      comments: [],
+      newComment: "",
     };
   },
   computed: {
@@ -61,16 +92,18 @@ export default {
       .then(({ data }) => {
         console.log(data)
         if (data.updatedat !== null) {
-          data.createdat = data.updatedat;
+          let a = data.updatedat.substring(0, 10);
+          let b = data.updatedat.substring(11, data.updatedat.length);
+          data.updatedat = a + " " + b;
+        } else {
+          let a = data.createdat.substring(0, 10);
+          let b = data.createdat.substring(11, data.createdat.length);
+          data.createdat = a + " " + b;
         }
-        let a = data.createdat.substring(0, 10);
-        let b = data.createdat.substring(11, data.createdat.length);
-        // console.log(a);
-        // console.log(b);
-        data.createdat = a + " " + b;
         this.article = data;
       })
-    console.log(this.userinfo)
+
+
     // this.$store.dispatch('memberStore/fetchUserinfo')
     //   .then(() => {
     //     // 사용자 정보를 가져왔을 때 필요한 작업 수행
@@ -81,6 +114,16 @@ export default {
     //     // 오류 처리
     //     console.error(error)
     //   });
+
+    axiosInstance.get('http://localhost/api/v1/comment/getlist/' + param)
+      .then((res) => {
+        this.comments = res.data;
+        console.log(this.comments);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
   },
   methods: {
     // ...mapActions("memberStore", ["decodeToken", "fetchUserinfo"]),
