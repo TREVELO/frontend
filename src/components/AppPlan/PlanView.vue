511889c08a4886c0a70bc1ec40f8d0a25ed8cf9d
이도하 <victoryddh5@naver.com>
2023-05-25 17:45:35 +0900

    plan view map

--- src/components/AppPlan/PlanView.vue
+++ src/components/AppPlan/PlanView.vue
@@ -14,6 +14,14 @@
                 </div>
             </div>
 
+            <div style="margin-bottom: 50px;">
+                <b-form-checkbox value="true" unchecked-value="false" v-for="(index, i) in day" :key="i"
+                    style="display: inline; margin: 10px;" v-model="check[index - 1]">
+                    {{ index }}일차
+                </b-form-checkbox>
+            </div>
+
+            <div id="map" style="margin: 0 auto;"></div>
 
             <div>
                 {{ plan.path }}
@@ -31,12 +39,41 @@ export default {
     data() {
         return {
             plan: {},
+            markerPositions: [],
+            marker: [],
+            day: '',
+            dayPlans: [],
+            check: [],
+        }
+    },
+    mounted() {
+        if (window.kakao && window.kakao.maps) {
+            this.initMap();
+        } else {
+            const script = document.createElement("script");
+            /* global kakao */
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src =
+                "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=68a4947f8091662e797250d4aa2d4a54";
+            document.head.appendChild(script);
         }
     },
     created() {
         this.getPlan();
+
     },
     methods: {
+        initMap() {
+            const container = document.getElementById("map");
+            const options = {
+                center: new kakao.maps.LatLng(35.19733010328186, 126.80823232697378),
+                level: 5,
+            };
+
+            //지도 객체를 등록합니다.
+            //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
+            this.map = new kakao.maps.Map(container, options);
+        },
         getPlan() {
             let param = this.$route.params.planId;
             console.log(param);
@@ -44,9 +81,14 @@ export default {
             axiosInstance.get('http://localhost/api/v1/plan/view/' + param)
                 .then((res) => {
                     this.plan = res.data;
-                    console.log(this.plan)
-
                     this.plan.path = JSON.parse(this.plan.path)
+                    this.day = this.plan.path.length;
+
+                    for (var index = 0; index < this.plan.path.length; index++) {
+                        this.dayPlans.push(this.plan.path[index])
+                        this.check.push("true")
+                    }
+
                     console.log(this.plan)
                 })
                 .catch((err) => {
@@ -66,9 +108,47 @@ export default {
                         console.log(err)
                     })
             }
-        }
+        },
+        displayMarker(markerPositions) {
+            // console.log(this.attractions)
+            console.log("처음 markerpositions")
+            console.log(this.markerPositions)
+            if (this.markers.length > 0) {
+                this.markers.forEach((marker) => marker.setMap(null));
+            }
+
+            // console.log(kakao);
+            const positions = markerPositions.map(
+                (position) => new kakao.maps.LatLng(...position)
+            );
+
+            // console.log("positions")
+            // console.log(positions)
+
+            if (positions.length > 0) {
+                this.markers = positions.map(
+                    (position) =>
+                        new kakao.maps.Marker({
+                            map: this.map,
+                            position,
+                        })
+                );
+
+                const bounds = positions.reduce(
+                    (bounds, latlng) => bounds.extend(latlng),
+                    new kakao.maps.LatLngBounds()
+                );
+
+                this.map.setBounds(bounds);
+            }
+        },
     }
 }
 </script>
 
-<style></style>
+<style scoped>
+#map {
+    width: 1000px;
+    height: 600px;
+}
+</style>
